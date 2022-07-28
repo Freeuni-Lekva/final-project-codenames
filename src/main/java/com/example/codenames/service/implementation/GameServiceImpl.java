b0b016580d7daf0180f00b8f8aaca520d321ff0a
KetiBego <kbegi20@freeuni.edu.ge>
2022-07-29 02:28:12 +0400

    GameService implementation added

---
+++ src/main/java/com/example/codenames/service/implementation/GameServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.codenames.service.implementation;
+
+import com.example.codenames.Model.Game;
+import com.example.codenames.dao.GameDao;
+import com.example.codenames.dto.GameInfoDto;
+import com.example.codenames.service.GameService;
+
+import java.util.Date;
+
+public class GameServiceImpl implements GameService {
+    private final GameDao gameDao;
+
+    public GameServiceImpl(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
+
+    @Override
+    public Game addGame(String winner, String loser, boolean blackWordSelected, Date date) {
+        GameInfoDto gameInfoDto = new GameInfoDto(winner, loser, blackWordSelected, date);
+        return gameDao.addGame(gameInfoDto);
+    }
+
+    @Override
+    public Game getGameByID(int gameID) {
+        return gameDao.getGameByID(gameID);
+    }
+}
