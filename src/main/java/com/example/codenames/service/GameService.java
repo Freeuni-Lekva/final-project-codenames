1db1a532284e14e3158dc9c29582fdddf20cb0a3
KetiBego <kbegi20@freeuni.edu.ge>
2022-07-28 22:10:42 +0400

    GameService interface added

---
+++ src/main/java/com/example/codenames/service/GameService.java
@@ -0,0 +1,9 @@
+package com.example.codenames.service;
+
+import com.example.codenames.Model.Game;
+import java.util.Date;
+
+public interface GameService {
+    Game addGame(String winner, String loser, boolean blackWordSelected, Date date);
+    Game getGameByID(int gameID);
+}
