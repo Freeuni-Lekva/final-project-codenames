6e7fdc75a4867eb30f03064b72dfe77d457d935a
mtkes20 <mtkes20@freeuni.edu.ge>
2022-09-05 00:18:44 +0400

    Scheme generation added

--- src/main/java/com/example/codenames/listener/NameConstants.java
+++ src/main/java/com/example/codenames/listener/NameConstants.java
@@ -2,10 +2,13 @@ package com.example.codenames.listener;
 
 public class NameConstants {
     public static final String USER_SERVICE = "USER_SERVICE";
+    public static final String WORD_SERVICE = "WORD_SERVICE";
     public static final String DB_CONNECTION = "DB_CONNECTION";
     public static final String USER_DAO = "USER_DAO";
     public static final String USERNAME_PARAMETER = "Username";
     public static final String PASSWORD_PARAMETER = "Password";
     public static final String REGISTRATION_ERROR = "Registration_Error";
     public static final String LOGIN_ERROR = "Login_Error";
+    public static final String BOARD = "BOARD";
+
 }
